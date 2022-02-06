17b04726a238ccb85407f5d8f21e79ea72037c1d
Shiielty <kazutonine@gmail.com>
2022-02-06 18:57:41 +0700

    Add basic math function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hi!");
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Cannot be divided by 0";
+  } else {
+    return a / b;
+  }
+}
